4e82149a09ce92691b106f6217d73d16a59f1608
Raushan Kumar <raushanrg69@gmail.coom>
2023-10-23 01:54:18 +0530

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,39 @@
+"Use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // We are using nodejs, not browser
+
+console.log(3 + 3); // code readability should be high
+
+console.log("Raushan")
+
+let name = "Raushan"
+let age = 18;
+let isLoggedIn = false
+let satate;
+
+
+
+// number => 2 to power 53 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbnol => unique
+
+
+// Object
+
+console.log(typeof undefined); // undifind
+console.log(typeof null); // object
+
+
+
+
+
+
+
+
+
+
+
