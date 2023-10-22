02999f4440b0ed1501181eb614b04a312c36add8
Raushan Kumar <raushanrg69@gmail.coom>
2023-10-23 01:54:32 +0530

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,22 @@
+const accountId = 144553;
+let accountEmail = "raushanrg69@gmail.com";
+var accountPassword = "122345";
+accountCity = "Delhi"
+let accountState;
+// accountId = 3; //not allowed
+accountEmail = "ranfn"
+accountPassword ="6575"
+accountCity = "Jaipur"
+
+
+console.log(accountId);
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.table([accountEmail, accountPassword, accountCity, accountState]);
+
+
+
