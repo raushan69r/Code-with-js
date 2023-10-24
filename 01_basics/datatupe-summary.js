876bd4ba316900315602ba21903e4775e2586f92
Raushan Kumar <raushanrg69@gmail.coom>
2023-10-24 14:08:40 +0530

    datatype

--- 01_basics/datatupe-summary.js
+++ 01_basics/datatupe-summary.js
@@ -2,17 +2,17 @@
 
 // 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 234874884434335435n
 
@@ -20,19 +20,43 @@ console.log(id === anotherId);
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
+// const heros = ["shaktiman", "naagraj", "doga"];
 
-let myObj = {
-    name: "Raushan",
-    age: 18
+// let myObj = {
+//     name: "Raushan",
+//     age: 18
+// }
+
+// const myFunction = function(){
+//     console.log("Herro word");
+// }
+
+// console.log(typeof myFunction);
+
+
+
+
+
+//***************************************************
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "code with harry"
+
+let anothername = myYoutubename
+anothername = "chaiorcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@gmail.coom",
+    upi: "user@ybl"
 }
 
-const myFunction = function(){
-    console.log("Herro word");
-}
+let userTwo = userOne
 
-console.log(typeof myFunction);
+userTwo.email = "user.com"
 
-
-
-
+console.log(userOne.email);
+console.log(userTwo.email);
